159f6f38e21c61dbca5b9fa57044e7857f4e9ea1
Francisco Panis Kaseker <franciscopk.ilax@gmail.com>
2021-03-30 10:12:50 -0300

    posix code

--- backup-library.sh
+++ backup-library.sh
@@ -332,7 +332,7 @@ checkRequiredPackages()
 
 logInvalidParametersNumber()
 {
-  if [ $parameters_count -ne $parameters_count_expected ]
+  if [[ $parameters_count -ne $parameters_count_expected ]]
   then
     log ERROR "Invalid parameters number. Exepected: >>> $parameters_count_expected <<<. Given: >>> $parameters_count <<<. Aborting with return code >>> 1 <<<."
   fi
